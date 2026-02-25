dfff8ac9d0330a3cf308fe8eeab2418ccadecb3b
ᔆ ᴾ ᴱ ᴱ ᴰ ™ ⌁ 🇵🇸 <roroxroot@gmail.com>
2026-02-25 20:57:59 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Codez",
+  description: "Your personal AI coding assistant.",
   appleWebApp: {
-    title: "NextChat",
+    title: "Codez",
     statusBarStyle: "default",
   },
 };
